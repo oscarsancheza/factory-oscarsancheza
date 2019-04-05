ea2840b6b74ca4fa8a6c46c0ebc267ad25eefdb3
oosancheza <oosancheza@concredito.com.mx>
2019-04-04 19:45:32 -0700

    [MOD] se agrego funcionalidad para que guarde formato csv

--- src/main/java/com/mcc/formateadorDatos/formatosArchivo/FormatoCsv.java
+++ src/main/java/com/mcc/formateadorDatos/formatosArchivo/FormatoCsv.java
@@ -1,6 +1,11 @@
 package com.mcc.formateadorDatos.formatosArchivo;
 
 import com.mcc.formateadorDatos.framework.Formato;
+import com.mcc.formateadorDatos.modelos.Alumno;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.List;
 
 public class FormatoCsv extends Formato {
 
@@ -9,7 +14,42 @@ public class FormatoCsv extends Formato {
   }
 
   @Override
-  public void generar() {
+  public void generar() throws IOException {
 
+    List<Alumno> alumnos = Alumno.getAlumnos();
+    StringBuilder datos = new StringBuilder();
+
+    if (!alumnos.isEmpty()) {
+
+      Field[] fields = alumnos.get(0).getClass().getDeclaredFields();
+
+      for (int i = 0; i < fields.length; i++) {
+        datos.append(fields[i].getName());
+        if (i != fields.length - 1) {
+          datos.append(",");
+        }
+      }
+
+      for (Alumno alumno : alumnos) {
+        datos.append("\n");
+        datos.append(alumno.getNoDeControl());
+        datos.append(",");
+        datos.append(alumno.getNombre());
+        datos.append(",");
+        datos.append(alumno.getApellidoPaterno());
+        datos.append(",");
+        datos.append(alumno.getApellidoMaterno());
+        datos.append(",");
+        datos.append(alumno.getEdad());
+        datos.append(",");
+        datos.append(alumno.getSexo());
+        datos.append(",");
+        datos.append(alumno.getCarrera());
+        datos.append(",");
+        datos.append(alumno.getSemestre());
+      }
+    }
+
+    this.guardarArchivo(datos.toString());
   }
 }
