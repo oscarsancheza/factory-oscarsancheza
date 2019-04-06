c76df650c985b04f17290c4cd647387679821294
oosancheza <oosancheza@concredito.com.mx>
2019-04-06 12:57:47 -0700

    [MOD] se agrego funcionalidad a formato txt

--- src/main/java/com/mcc/formateadorDatos/FormateadorDatosApplication.java
+++ src/main/java/com/mcc/formateadorDatos/FormateadorDatosApplication.java
@@ -9,7 +9,7 @@ public class FormateadorDatosApplication {
 
   public static void main(String[] args) {
     FormateadorDatos formateadorDatos = new FormatoArchivo();
-    Formato formato = formateadorDatos.formatearDatos(TipoFormato.CSV, "alumnos");
+    Formato formato = formateadorDatos.formatearDatos(TipoFormato.TXT, "alumnos");
     System.out.println("Nombre archivo con formato:" + formato.getNombreArchivo());
   }
 }
